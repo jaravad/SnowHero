be0fe910eb7f873264909fe13117a9398fe42d52
jesu2607 <jaravad@uninorte.edu.co>
2019-12-07 21:52:21 -0500

    'our service' section added

--- download_images.py
+++ download_images.py
@@ -1,16 +1,16 @@
 import requests
 import bs4
 
-response = requests.get('https://snow-devil.myshopify.com/collections/jackets')
+response = requests.get('https://snow-devil.myshopify.com/collections/jackets?page=2')
 
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
 img_tags = soup.select('.grid-uniform img')
 #/media/jesusdavid/Windows/Users/Jesus_David/Desktop/Web Development/SnowHero/images
-i = 1
+i = 19
 for jacket in img_tags:
 	r = requests.get('https:'+jacket['src'])
-	with open('/media/jesusdavid/Windows/Users/Jesus_David/Desktop/Web Development/SnowHero/images/'+'jacket_'+str(i)+'.jpg', 'wb') as f:
+	with open('C:/Users/Jesus_David/Desktop/Web Development/SnowHero/images/jackets/'+'jacket_'+str(i)+'.jpg', 'wb') as f:
 		f.write(r.content)
 	
 	i+=1
